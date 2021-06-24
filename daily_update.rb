412a83de07a2cc2f3ad92c70f04bc689fae0fe9b
Douglas Amport <douglas.amport@gmail.com>
2021-06-24 07:34:43 -0400

    completed sum methods

--- daily_update.rb
+++ daily_update.rb
@@ -6,23 +6,54 @@ spreadsheet = session.spreadsheet_by_title("Stock Test Spreadsheet")
 worksheet_one = spreadsheet.worksheets.first
 worksheet_two = spreadsheet.worksheets[1]
 
-
 date = Time.new.strftime("%m-%d-%Y")
-
 
 worksheet_one.export_as_file( "#{date}.csv")
 
-# move file into directory
-#check number of files in directory & remove old ones after 10 or so
-
-def make_dir
- unless Dir.exist?
-  Dir.mkdir(snapshot)
-  
+def make_dir(dir_name)
+  unless Dir.exist?(dir_name)
+    Dir.mkdir(dir_name)
+  end
 end
 
-# Summarize total of stocks and total of crypto 
-# assign to correct columns of Worksheet Two.
+make_dir("snapshot")
+File.rename("#{date}.csv", "snapshot/#{date}.csv")
 
-stock_total = 
-crypto_total = 
+def sum(worksht, rtrn = nil , crypto_sum = [], stock_sum = [])
+  worksht.rows.each do |row|
+    if row[1] == "CRYPTO"
+      crypto_sum << row[4] 
+    else
+      stock_sum << row[4]
+    end
+  end
+  arr = rtrn == 'crypto' ? crypto_sum : stock_sum
+  arr = format_arr(arr)
+  arr.sum
+end
+
+def format_arr(arr)
+  arr.map do |val|
+    val.gsub!(/\$|\,/, '')
+    val.to_f
+  end
+end
+
+# sum up the crypto total and the stock total and print to second sheet of google doc.
+
+crypto_total = sum(worksheet_one, "crypto")
+stock_total = sum(worksheet_one)
+
+worksheet_two.insert_rows(worksheet_two.num_rows + 1, [["#{date}", "#{stock_total}", "#{crypto_total}", "#{stock_total + crypto_total}"]])
+worksheet_two.save
+
+# Limit the Number of daily snapshots to 10 to save storage
+
+def limit_dir_size(dir_name, num)
+  files = Dir.glob("#{dir_name}/*")
+  if files.length > num
+    File.delete(files[0])
+  end
+end
+
+limit_dir_size('snapshot', 10)
